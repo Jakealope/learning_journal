fa0b968da007028c9b47748dff818b9534105363
Jakealope <jake.anderson486@gmail.com>
2015-02-19 15:32:20 -0800

    moved js to new file

---
+++ static/journal.js
@@ -0,0 +1,12 @@
+
+$( document ).ready( function() {
+  $("#edit_form").hide();
+
+$("#btn_edit_post").on("click", function() {
+    $("#edit_detail").hide(1500);
+    $("#edit_form").fadeIn(2500);
+});
+$("#btn_submit_edit").on("click", function() {
+    $()
+});
+});
